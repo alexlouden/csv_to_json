a4fef35029899a4e48a7acf141c78c83b0c38c1b
Alex Louden <alex@louden.com>
2015-02-04 12:43:51 +0800

    Handles json in both dict of lists and list of dicts form

--- json_to_csv.py
+++ json_to_csv.py
@@ -7,17 +7,25 @@ from collections import OrderedDict
 def json_to_csv(input_f, output_f):
     """ Convert JSON to CSV """
 
-    dict_of_lists = json.load(input_f, object_pairs_hook=OrderedDict)
+    input_data = json.load(input_f, object_pairs_hook=OrderedDict)
 
-    # Dict of lists to list of dicts
-    # Assume all lists are same length
-    list_len = len(dict_of_lists.values()[0])
-    list_of_dicts = [
-        {k: v[i] for k, v in dict_of_lists.items()}
-        for i in range(list_len)
-    ]
+    if isinstance(input_data, list):
+        list_of_dicts = input_data
+        fieldnames = list_of_dicts[0].keys()
 
-    w = csv.DictWriter(output_f, dict_of_lists.keys(), lineterminator='\n')
+    elif isinstance(input_data, dict):
+        dict_of_lists = input_data
+        fieldnames = dict_of_lists.keys()
+
+        # Dict of lists to list of dicts
+        # Assume all lists are same length
+        list_len = len(dict_of_lists.values()[0])
+        list_of_dicts = [
+            {k: v[i] for k, v in dict_of_lists.items()}
+            for i in range(list_len)
+        ]
+
+    w = csv.DictWriter(output_f, fieldnames, lineterminator='\n')
     w.writeheader()
     w.writerows(list_of_dicts)
 
